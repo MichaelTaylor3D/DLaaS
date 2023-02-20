3a54b81c1aa8ae588035a9fde30d257a2f2ebc0a
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-02-20 15:02:41 -0500

    feat: create user api

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_access_key"         {}
-variable "aws_secret_key"         {}
+variable "aws_secret_key"         { }
 variable "aws_region"             { default = "us-east-1" }
 variable "aws_profile"            { default = "dlstorage_services" }
 variable "default_storage_bucket" { default = "dlstorage-services" }
