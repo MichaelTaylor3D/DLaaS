ec446ccf1bc8ab9f659049bfa350292b9ac511f4
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-02-20 08:30:07 -0500

    ci: add deplyment script

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.aws_region
-  profile    = var.aws_profile
 }
 
 #module "service-job-queue" {
