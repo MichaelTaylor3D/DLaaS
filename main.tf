7f356f06509ca5491e769dc96504cc345e61ed54
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-02-20 07:47:39 -0500

    ci: add deplyment script

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-
-  backend "remote" {
+  cloud {
     organization = "TaylorDigitalServices"
 
     workspaces {
