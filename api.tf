e43e89443e8d66356853fdb214cce49f45a706f1
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-02-20 15:07:53 -0500

    feat: create user api

--- api.tf
+++ api.tf
@@ -1,6 +1,6 @@
 # API Gateway REST API definition
 resource "aws_api_gateway_rest_api" "main" {
-  name        = "datalayer-services-api"
+  name        = "datalayer-storage-services-api"
   description = "${var.aws_profile} services API [Deployed: ${timestamp()}]"
 }
 
