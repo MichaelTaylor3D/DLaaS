8586b7c44091f0d8e3279c36c1d09b6a7d1b0e04
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-02-20 14:54:41 -0500

    feat: create user api

--- utils/npm-install-all.js
+++ utils/npm-install-all.js
@@ -1,37 +1,36 @@
-var fs = require('fs');
-var resolve = require('path').resolve;
-var join = require('path').join;
-var cp = require('child_process');
-var os = require('os');
+var fs = require("fs");
+var resolve = require("path").resolve;
+var join = require("path").join;
+var cp = require("child_process");
+var os = require("os");
 
 // get library path
-var lib = resolve(__dirname, '../modules');
+var lib = resolve(__dirname, "../modules");
 
 const installRecursively = (dir) => {
   fs.readdirSync(dir).forEach(function (mod) {
     var modPath = join(dir, mod);
 
-    console.log('Checking path for npm install', modPath);
-
     // ensure path has package.json
-    if (!fs.existsSync(join(modPath, 'package.json'))) {
-      const lambdaPath = join(modPath, 'lambdas');
+    if (!fs.existsSync(join(modPath, "package.json"))) {
+      const lambdaPath = join(modPath, "lambdas");
+
       if (fs.existsSync(lambdaPath)) {
         installRecursively(lambdaPath);
       }
       return;
     }
 
-    console.log(`Building module at path: ${modPath}`);
+    console.log(`Installing module at path: ${modPath}`);
 
     // npm binary based on OS
-    var npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm';
+    var npmCmd = os.platform().startsWith("win") ? "npm.cmd" : "npm";
 
     // install folder
-    cp.spawn(npmCmd, ['i'], {
+    cp.spawn(npmCmd, ["install"], {
       env: process.env,
       cwd: modPath,
-      stdio: 'inherit',
+      stdio: "inherit",
     });
   });
 };
