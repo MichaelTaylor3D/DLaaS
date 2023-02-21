df1c8c287fd8e88bf7edcca99eccd4dfe2016192
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-02-21 11:46:55 -0500

    refactor: revert to use usermata for salt

--- modules/service.user/lambdas/login/index.js
+++ modules/service.user/lambdas/login/index.js
@@ -65,10 +65,11 @@ const getSaltAndHashForUser = async (username) => {
 
   return new Promise((resolve, reject) => {
     const sql = `
-      SELECT user_meta.salt, users.id as user_id, users.password_hash as hash, user.confirmed as confirmed
+      SELECT user_meta.meta_value, users.id as user_id, users.password_hash as hash, user.confirmed as confirmed
       FROM users
       INNER JOIN user_meta on users.id = user_meta.user_id
-      WHERE users.username = :username;
+      WHERE users.username = :username
+      AND user_meta.meta_key = 'salt';
     `;
 
     const params = { username };
