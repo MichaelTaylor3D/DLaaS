c11bd8b2a071d197d908ec8e80c9c1a594537bda
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-02-22 17:51:34 -0500

    feat: add worker gateway

--- modules/service.worker-gateway/variables.tf
+++ modules/service.worker-gateway/variables.tf
@@ -1,4 +1,5 @@
 variable "aws_access_key"              {}
 variable "aws_secret_key"              {}
 variable "aws_region"                  {}
+variable "aws_profile"                 {}
 variable "default_lambda_role_arn"     {}
