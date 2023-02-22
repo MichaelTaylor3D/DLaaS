083079911734513174fe50db9e659897cc04bb58
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-02-22 17:59:01 -0500

    feat: add worker gateway

--- modules/service.worker-gateway/sqs.tf
+++ modules/service.worker-gateway/sqs.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue" "fifo_queue" {
   fifo_queue                        = true
-  name                              = "worker-gateway-message-handler"
+  name                              = "worker_gateway_message_handler"
   sqs_managed_sse_enabled           = true
 }
