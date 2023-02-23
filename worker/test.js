f7708cbdb0217a23ac9b8a9ce27665a654ac5862
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-02-23 17:56:32 -0500

    test: sqs ws

---
+++ worker/test.js
@@ -0,0 +1,52 @@
+const WebSocket = require("ws");
+const { Consumer } = require("sqs-consumer");
+const { SQSClient } = require("@aws-sdk/client-sqs");
+
+const app = Consumer.create({
+  queueUrl:
+    "https://sqs.us-east-1.amazonaws.com/873139760123/worker-gateway-message-handler.fifo",
+  messageAttributeNames: ["All"],
+  handleMessage: async (message) => {
+    console.log(message);
+
+    const response = {
+      MessageGroupId:  message.MessageAttributes.connectionId.StringValue,
+      connectionId: message.MessageAttributes.connectionId.StringValue,
+      requestId: message.MessageAttributes.requestId.StringValue,
+      message: message.Body,
+    };
+
+    console.log(response);
+
+    const exampleSocket = new WebSocket(
+      "wss://cp7m4w1cm9.execute-api.us-east-1.amazonaws.com/production"
+    );
+
+    exampleSocket.onopen = (event) => {
+      console.log("!");
+      exampleSocket.send(JSON.stringify(response));
+      exampleSocket.close();
+    };
+  },
+  sqs: new SQSClient({
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: "AKIA4WSZPNP52LY2IJDV",
+      secretAccessKey: "MPnglkg4FVhEBJXTQjl4weU5WQFvYE6DvBWCKkDq",
+    },
+  }),
+});
+
+app.on("error", (err) => {
+  console.error(err.message);
+});
+
+app.on("processing_error", (err) => {
+  console.error(err.message);
+});
+
+app.on("timeout_error", (err) => {
+  console.error(err.message);
+});
+
+app.start();
