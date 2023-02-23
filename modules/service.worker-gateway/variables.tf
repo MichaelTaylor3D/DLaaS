dc3e71da3b5b64846ad4f3c7353fc0e5ef06f349
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-02-23 17:58:00 -0500

    test: sqs ws

--- modules/service.worker-gateway/variables.tf
+++ modules/service.worker-gateway/variables.tf
@@ -1,6 +1,6 @@
 variable "aws_access_key"              {}
 variable "aws_secret_key"              {}
-variable "aws_region"                  {}
+#variable "aws_region"                  {}
 variable "aws_profile"                 {}
 variable "default_lambda_role_arn"     {}
 variable "dev_bucket_id"               {}
