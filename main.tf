fe294b49630d8a34eab605fa253b83b701652d05
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-03-01 11:04:47 -0500

    feat: add worker gateway service

--- main.tf
+++ main.tf
@@ -45,15 +45,27 @@ module "service-user" {
 module "service-worker-gateway" {
   source                      = "./modules/service.worker-gateway"
 
-    # AWS Profile
+  # AWS Profile
   aws_access_key              = var.aws_access_key
   aws_secret_key              = var.aws_secret_key
   aws_region                  = var.aws_region
   aws_profile                 = var.aws_profile
 
+  # Storage
+  storage_bucket_id           = aws_s3_bucket.storage-bucket.id
+  dev_bucket_id               = aws_s3_bucket.storage-devops-bucket.id
+
+  # Network
+  public_subnet_id            = aws_subnet.public-2.id
+
   # Policies And Roles
   default_lambda_role_arn     = aws_iam_role.default-lambda-role.arn
+  
+  # API Root
+  api_gateway_id              = aws_api_gateway_rest_api.main.id
+  root_resource_id            = aws_api_gateway_rest_api.main.root_resource_id
+  api_gateway_arn             = aws_api_gateway_rest_api.main.execution_arn
 
-  # Storage
-  dev_bucket_id               = aws_s3_bucket.storage-devops-bucket.id
+  # EC2 - Compute
+  worker_ami                  = var.worker_ami
 }
