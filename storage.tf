d4188e6c806155c08738df2cc3f7dd734a6e2288
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-05-17 22:38:06 -0400

    fix: modify how access policy is added to main bucket

--- storage.tf
+++ storage.tf
@@ -35,21 +35,6 @@ resource "aws_s3_bucket" "storage-bucket" {
   force_destroy       = true
   acceleration_status = "Enabled"
 
-  policy              = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "PublicReadGetObject",
-        "Effect": "Allow",
-        "Principal": {
-          "AWS": "*"
-        },
-        "Action": "s3:GetObject",
-        "Resource": "arn:aws:s3:::${local.config.DEFAULT_S3_BUCKET}/*"
-      }
-    ]
-  })
-
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
@@ -72,3 +57,28 @@ resource "aws_s3_bucket_public_access_block" "access_block" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.storage-bucket.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "PublicReadGetObject",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "*"
+        },
+        "Action": "s3:GetObject",
+        "Resource": "arn:aws:s3:::${local.config.DEFAULT_S3_BUCKET}/*"
+      }
+    ]
+  })
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.access_block
+  ]
+}
+
+
