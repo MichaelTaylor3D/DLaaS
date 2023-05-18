57dbd77072a3993d80b4a5dfca4be9a87b85feaa
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-05-17 20:34:21 -0400

    fix: move common-layer folder creation to github ci

--- common-layer.tf
+++ common-layer.tf
@@ -7,61 +7,10 @@
 
 resource "random_uuid" "archive" { }
 
-data "local_file" "common_files" {
-  for_each = fileset("${path.module}/common", "*")
-
-  filename = "${path.module}/common/${each.value}"
-}
-
-locals {
-  common_files_sha256 = sha256(join("", [for f in data.local_file.common_files : f.content_base64]))
-}
-
-resource "null_resource" "copy_common_files_to_layer" {
-  provisioner "local-exec" {
-    command = <<EOF
-      mkdir -p ${path.module}/common-layer/nodejs/common && \
-      cp -R ${path.module}/common/* ${path.module}/common-layer/nodejs/common && \
-      cp -R ${path.module}/common/node_modules ${path.module}/common-layer/nodejs && \
-      rm -rf ${path.module}/common-layer/nodejs/common/node_modules && \
-      touch ${path.module}/complete.txt
-    EOF
-  }
-
-  triggers = {
-    # someone please help me find a way to build this common layer,
-    # without having to rebuild it every deployment 
-    rebuild_trigger = sha1(timestamp())
-  }
-
-  depends_on = [
-    data.local_file.common_files,
-  ]
-}
-
-resource "null_resource" "wait_for_file" {
-  provisioner "local-exec" {
-    command = <<EOF
-      until [ -f ${path.module}/complete.txt ]
-      do
-        sleep 1
-      done
-    EOF
-  }
-
-  triggers = {
-    rebuild_trigger = sha1(timestamp())
-  }
-}
-
 data "archive_file" "lambda_layer_source" {
   type        = "zip"
   source_dir  = "${path.module}/common-layer"
   output_path = "${path.module}/common-layer-tf-handler-${random_uuid.archive.result}.zip"
-
- depends_on = [
-    null_resource.wait_for_file
-  ]
 }
 
 resource "aws_lambda_layer_version" "common_layer" {
