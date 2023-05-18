82356a958fea7c3dc9b0ccac57852902cdd74849
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-05-17 20:46:21 -0400

    ci: fix build commands

--- common-layer.tf
+++ common-layer.tf
@@ -13,7 +13,7 @@ data "archive_file" "lambda_layer_source" {
   output_path = "${path.module}/common-layer-tf-handler-${random_uuid.archive.result}.zip"
 }
 
-resource "aws_lambda_layer_version" "common_layer" {
+resource "aws_lambda_layer_version" "common-layer" {
   filename           = data.archive_file.lambda_layer_source.output_path
   source_code_hash   = data.archive_file.lambda_layer_source.output_base64sha256
   layer_name         = "common-layer"
